be0256d31bff3dd0984ceac93065d4e4a37bccb6
Ivan11217 <ivannunez@uees.edu.ec>
2024-07-11 07:17:58 -0500

    Segundo parcial

---
+++ models/etiqueta.js
@@ -0,0 +1,26 @@
+'use strict';
+const { Model } = require('sequelize');
+const Foto = require('../models').foto;
+const Etiqueta = require('../models').etiqueta;
+
+module.exports = (sequelize, DataTypes) => {
+  class etiqueta extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.etiqueta.belongsToMany(models.foto, { through: 'fotoetiquetas', foreignKey: "etiqueta_id" });
+    }
+  }
+  etiqueta.init({
+    texto: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'etiqueta',
+    tableName: 'etiquetas'
+  });
+  return etiqueta;
+};
